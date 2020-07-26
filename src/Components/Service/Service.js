6b59491a34c5d9306a6acfcd29b9c6968c4b56c6
Santiago J <santiago.jimenez_rio@uao.edu.co>
2020-07-26 11:26:52 -0500

    Añadido pagina servicios

---
+++ src/Components/Service/Service.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Service.css';
+
+const Service = ({stImage, stTitle, stDescription, numOrientation}) => {
+    
+    const order={order: numOrientation}
+
+    return (
+        <div class="o-image-container">
+            <img className="o-image" src={stImage} />
+            <div class="o-info">
+                <h1 className="o-service-title">{stTitle}</h1>
+                <div className="o-service-description" style={order}>{stDescription}</div>
+            </div>
+        </div>
+    );
+}
+
+export default Service;
