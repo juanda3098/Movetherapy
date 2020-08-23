0d67a17edbc941aa307fc1c41acb31aa4fd33659
Santiago J <santiago.jimenez_rio@uao.edu.co>
2020-08-23 01:19:13 -0500

    -Mejorado AboutUs -Insta

--- src/ComponentsH/AboutUs/AboutUs.js
+++ src/ComponentsH/AboutUs/AboutUs.js
@@ -4,7 +4,13 @@ import Profile from "../../Img/person.svg";
 
 import "./AboutUs.css";
 
-import MTFront from "../../Img/MoveTherapyFront.png"
+import MTFront from "../../Img/MTFront-AboutUs.png"
+import ProfileDiana from "../../Img/People/DianaMesa.jpg"
+import ProfileLauraG from "../../Img/People/LauraGonzalez.jpg"
+import ProfileCamilo from "../../Img/People/CamiloUseche.jpg"
+import ProfileAndres from "../../Img/People/AndresPenagos.jpg"
+import ProfileLauraV from "../../Img/People/LauraVargas.jpg"
+import ProfileEider from "../../Img/People/EiderArce.jpg"
 
 const AboutUs = () => {
   return (
@@ -29,19 +35,44 @@ const AboutUs = () => {
         <h1 className="o-title-sentence">Nuestro Equipo</h1>
         <div className="o-aboutus-persons">
           <div className="o-aboutus-person">
-            <h2>Diana Marcela Mesa Sepúlveda</h2>
-            <img src={Profile} className="o-aboutus-img" alt="" />
-            <p>Fisioterapeuta</p>
+            <img src={ProfileDiana} className="o-aboutus-person-img" alt="" />
+            <h2>Diana Mesa</h2>
+            <h4>Fisioterapeuta</h4>
             <p>Escuela Nacional del Deporte</p>
           </div>
           <div className="o-aboutus-person">
-            <h2>Laura Marcela Gonzáles Sánchez</h2>
-            <img src={Profile} className="o-aboutus-img" alt="" />
-            <p>Fisioterapeuta</p>
+            <img src={ProfileLauraG} className="o-aboutus-person-img" alt="" ></img>
+            <h2>Laura Gonzáles</h2>
+            <h4>Fisioterapeuta</h4>
+            <p>Escuela Nacional del Deporte</p>
+          </div>
+          <div className="o-aboutus-person">
+            <img src={ProfileCamilo} className="o-aboutus-person-img" alt="" ></img>
+            <h2>Camilo Useche</h2>
+            <h4>Servicio Al Cliente</h4>
+            <p>Estudiante de Derecho</p>
+            <p>Universidad Santiago de Cali</p>
+          </div>
+          <div className="o-aboutus-person">
+            <img src={ProfileAndres} className="o-aboutus-person-img" alt="" ></img>
+            <h2>Andrés Penagos</h2>
+            <h4>Fisioterapia y Master Oficial en Fisioterapia Invasiva</h4>
+            <p>Escuela Nacional del Deporte</p>
+          </div>
+          <div className="o-aboutus-person">
+            <img src={ProfileLauraV} className="o-aboutus-person-img" alt="" ></img>
+            <h2>Laura Vargas</h2>
+            <h4>Nutricionista</h4>
+            <p>Escuela Nacional del Deporte</p>
+          </div>
+          <div className="o-aboutus-person">
+            <img src={ProfileEider} className="o-aboutus-person-img" alt="" ></img>
+            <h2>Eider Arce</h2>
+            <h4>Profesional en Deporte, Especialista en Actividad Física</h4>
             <p>Escuela Nacional del Deporte</p>
           </div>
         </div>
-        <h3>
+        <h3 className="o-bottom-text-people">
           Profesionales comprometidos con el bienestar físico y emocional de sus
           usuarios, los cuales se encuentran constantemente actualizados y a la
           vanguardia en nuevas técnicas de rehabilitación, dispuestos a
