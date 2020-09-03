02a3e5fea149a6a121ec972395f1f833509d861e
juanda3098 <juandapore3098@gmail.com>
2020-09-03 14:13:31 -0500

    Se adelantó la vistas de Admin

--- src/ComponentsA/Admin/Admin.js
+++ src/ComponentsA/Admin/Admin.js
@@ -3,9 +3,11 @@ import React from "react";
 // import { BrowserRouter, Switch, Route, useRouteMatch } from "react-router-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Menu from "../MenuA/MenuA";
-import Users from "../UsersA/UsersA";
-import Exercise from "../ExerciseA/ExerciseA";
+import Menu from "../Menu/Menu";
+import Users from "../Users/Users";
+import Exercise from "../Exercise/Exercise";
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
 
 import "./Admin.css";
 
@@ -14,15 +16,28 @@ function Admin() {
 
   return (
     <BrowserRouter>
-      <Menu />
-      <Switch>
-        {/* <Route path={`${path}/Users`} exact component={UsersA} />
+      <div className="o-admin-container">
+        <div className="o-admin-menu">
+          <Menu />
+        </div>
+
+        <div className="o-admin-field">
+          {/* <div className="o-admin-fild-header">
+            
+          </div> */}
+          <Header />
+          
+          <Switch>
+            
+            {/* <Route path={`${path}/Users`} exact component={UsersA} />
         <Route path={`${path}/Exercise`} exact component={ExerciseA} /> */}
 
-        <Route path="/Admin/Users" exact component={Users} />
-        <Route path="/Admin/Exercise" exact component={Exercise} />
-
-      </Switch>
+            <Route path="/Admin/Users" exact component={Users} />
+            <Route path="/Admin/Exercise" exact component={Exercise} />
+          </Switch>
+          <Footer />
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/ComponentsA/Users/Users.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Users.css'
+
+function Users() {
+
+    return(
+        <div>
+            Users
+        </div>
+    )
+}
+
+export default Users
