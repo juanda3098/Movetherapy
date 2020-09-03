8c25117bcf11f9e50a24ef56505d6f97f10ce8db
juanda3098 <juandapore3098@gmail.com>
2020-09-02 20:11:50 -0500

    Implementacion de la vista de admin

---
+++ src/ComponentsA/UsersA/UsersA.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './UsersA.css'
+
+function UsersA() {
+
+    return(
+        <div>
+            Users
+        </div>
+    )
+}
+
+export default UsersA
---
+++ src/Main/Main.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Page from "../ComponentsH/Page/Page";
+import Admin from '../ComponentsA/Admin/Admin';
+
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Page} />
+        <Route path="/Admin" exact component={Admin} />
+        {/* <Route path="/User" exact component={User} /> */}
+
+        {/* <Route path="/Profile" component={() => <Profile user={this.state.user} />} /> */}
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Main;
