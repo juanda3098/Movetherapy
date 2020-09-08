714f569c076d75c0e274e930cfa7dd37f2771e45
juanda3098 <juandapore3098@gmail.com>
2020-09-08 09:48:09 -0500

    Adelanto de la vista admin

--- src/ComponentsA/Admin/Admin.js
+++ src/ComponentsA/Admin/Admin.js
@@ -5,22 +5,14 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Menu from "../Menu/Menu";
+import Welcome from '../Welcome/Welcome';
 import Patients from "../Patients/Patients";
 import Exercise from "../Exercise/Exercise";
 import Header from "../Header/Header";
 
-// import Logo from '../../Img/Admin/Logo2.png'
-
 import "./Admin.scss";
 
 function Admin() {
-
-  // const [admin, setaAdmin] = useState("Diana Mesa");
-
-  // const outputEvents = () => {
-  //   setaAdmin(admin)
-  // }
-
   return (
     <BrowserRouter>
       <div className="o-admin-container">
@@ -31,12 +23,7 @@ function Admin() {
           <Header />
           <div className="o-admin-body">
             <Switch>
-              {/* <div className="o-body-welcome">
-                <img src={Logo} alt="" className="o-img-welcome" />
-                <h1 className="o-title-welcome">Bienvenido al panel</h1>
-                <h1 className="o-title-welcome">administrativo</h1>
-                <p>{admin}</p>
-              </div> */}
+              <Route path="/Admin" exact component={Welcome} />
               <Route path="/Admin/Patient" exact component={Patients} />
               <Route path="/Admin/Exercise" exact component={Exercise} />
             </Switch>
